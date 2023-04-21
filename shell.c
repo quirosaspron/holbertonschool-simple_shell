dc90553bb8607f9edb03423140fdd6f092498dae
Mateo <mateoquirosaspron@gmail.com>
2023-04-20 18:44:48 -0600

    It prints and takes input but still does not execute command

--- shell.c
+++ shell.c
@@ -1,26 +1,55 @@
 #include "shell.h"
-/*
+/**
  * main - it is a shell
  * Retun: void
- **/
+ */
 
-void main()
+int main(void)
 {
 	size_t n = 10;
-        char *buffer = NULL;
-	char cmd[];
+	char *buffer = NULL;
 	char *token;
-	/*Create argv*/
+	char *argv[10];
+	int argc = 0;
+	pid_t pid;
 
-        printf("#myshell$");
-        getline(&buffer, &n, stdin);
-	token = strtok(str, " ");        
-	/*input tokens into argv*/
-	/*Create a child process */
-	/* The child process will execve from the parameters in argv */
-	/*We will wait for the child to finish so we can print and start again */
+	while (1)
+	{
+		printf("#myshell$ ");
+		if (getline(&buffer, &n,stdin) == -1)
+		{
+			printf("Error reading input\n");
+			return (1);
+		}
 
-        free(buffer);
-        return (0);
-	
+		token = strtok(buffer, " ");
+		while (token != NULL)
+		{
+			argv[argc++] = token;
+			token = strtok(NULL, " ");
+		}
+
+		if (argc > 0)
+		{
+			pid = fork();
+			if (pid == -1)
+			{
+				printf("Error\n");
+				return 1;
+			}
+			else if (pid == 0)
+			{
+				execve(argv[0], argv, NULL );
+				return (1);
+			}
+			else
+			{
+				wait(NULL);
+			}
+		}
+		argc = 0;
+	}
+
+	free(buffer);
+	return (0);
 }
--- shell.h
+++ shell.h
@@ -1,4 +1,10 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
 #endif
