7dfb8d9e59f2e2a808b06386e4fb6acad98500bf
Mateo <mateoquirosaspron@gmail.com>
2023-04-29 09:51:35 -0600

    should work

--- shell.c
+++ shell.c
@@ -1,61 +1,117 @@
-#include "shell.h"
+#include "simple_shell.h"
 /**
- * main - it is a shell
- * Retun: void
+ * main - print prompt, handle EOF, read file_stream
+ * @argc: arg count (not needed)
+ * @argv: argv for command
+ * Return: Always 0.
  */
 
-int main(void)
+int main(int argc, char *argv[])
 {
-	size_t n = 10;
-	char *buffer = NULL;
-	char *token;
-	char *argv[10];
-	int argc = 0;
-	int val;
-	pid_t pid;
+	char *s = NULL;
+	size_t buffer_size = 0;
+	ssize_t file_stream = 0;
+	char *name;
+	(void) argc;
+	name = argv[0];
 
 	while (1)
 	{
-		/*printf("#myshell$ ");*/
-		if (getline(&buffer, &n,stdin) == -1)
+		if (isatty(STDIN_FILENO) == 1)
+			write(1, "$ ", 2);
+		file_stream = getline(&s, &buffer_size, stdin);
+		if (file_stream == -1)
 		{
-			/*printf("Error reading input\n");
-			perror("Error");*/
-			return (1);
+			if (isatty(STDIN_FILENO) == 1)
+				write(1, "\n", 1);
+			break;
 		}
-		token = strtok(buffer, " \n");
-		while (token != NULL)
-		{
-			argv[argc++] = token;
-			token = strtok(NULL, " \n");
-		}
-		argv[argc] = NULL;
-		if (argc > 0)
-		{
-			pid = fork();
-			if (pid == -1)
-			{
-				printf("Error\n");
-				return 1;
-			}
-			else if (pid == 0)
-			{
-				val = execve(argv[0], argv, NULL );
-				if (val == -1)
-				{
-					perror("Error");
-				}	
-				return (1);
-			}
-			else
-			{
-				wait(NULL);
-			}
-		}
-		argc = 0;
-		/*printf("#myshell$ ");*/
-	}
 
-	free(buffer);
+		if (s[file_stream - 1]  == '\n')
+			s[file_stream - 1]  = '\0';
+		if (*s == '\0')
+			continue;
+		if (cmd_read(s, file_stream) == 2)
+			break;
+	}
+	free(s);
+	s = NULL;
+	return (0);
+}
+/**
+ * cmd_read - handles command line and tokenizes it
+ *@s: string
+ *@file_stream: getline input
+ * Return: 0
+ */
+int cmd_read(char *s, size_t __attribute__((unused))file_stream)
+{
+	char *token = NULL;
+	char *cmd_arr[100];
+	int i;
+
+	if (_strcmp(s, "exit") == 0)
+		return (2);
+	if (_strcmp(s, "env") == 0)
+		return (_printenv());
+	token = strtok(s, " "), i = 0;
+	while (token)
+	{
+		cmd_arr[i++] = token;
+		token = strtok(NULL, " ");
+	}
+	cmd_arr[i] = NULL;
+/* Return status code */
+	return (call_command(cmd_arr));
+}
+/**
+ * print_not_found - prints when cmd is not found in path
+ *
+ * @cmd: a string provided by the stdin
+ */
+void print_not_found(char *cmd)
+{
+	char *name;
+
+	write(2, name, _strlen(name));
+	write(2, ": 1: ", 5);
+	write(2, cmd, _strlen(cmd));
+	write(2, ": not found\n", 12);
+}
+/**
+ * call_command - calls cmd, forks, execve
+ *
+ * @cmd_arr: a string provided by the stdin
+ * Return: 0
+ */
+int call_command(char *cmd_arr[])
+{
+	char *exe_path_str = NULL;
+	char *cmd = NULL;
+	pid_t is_child;
+	int status;
+
+	cmd = cmd_arr[0];
+	exe_path_str = pathfinder(cmd);
+	if (exe_path_str == NULL)
+	{
+		print_not_found(cmd);
+		return (3);
+	}
+	is_child = fork();
+	if (is_child < 0)
+	{
+		perror("Error:");
+		return (-1);
+	}
+	if (is_child > 0)
+		wait(&status);
+	else if (is_child == 0)
+	{
+		(execve(exe_path_str, cmd_arr, environ));
+		perror("Error:");
+		exit(1);
+	}
+	free(exe_path_str);
 	return (0);
 }
