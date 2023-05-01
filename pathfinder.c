9f2b35a83f7a50cb908f3a7352a2fa129abd61e9
Mateo <mateoquirosaspron@gmail.com>
2023-04-30 22:06:37 -0600

    a test

--- pathfinder.c
+++ pathfinder.c
@@ -2,9 +2,9 @@
 char *pathfinder(char *cmd)
 {
     struct stat buf;
-    char *path;
-    char *path_tokens;
-    char *path_array[100];
+    char *path = NULL;
+    char *path_tokens = NULL;
+    char *path_array[100] = {NULL};
     char *s2 = cmd;
     char *new_path = NULL;
     int i, j;
@@ -16,9 +16,17 @@ char *pathfinder(char *cmd)
     }
 
     path = _strdup(_getenv("PATH"));
+    if (path == NULL)
+        return (NULL);
+
     path_tokens = strtok(path, ":");
 
-    new_path = malloc(sizeof(char) * 100);
+    new_path = malloc(sizeof(char) * (strlen(path) + strlen(cmd) + 2));
+    if (new_path == NULL)
+    {
+        free(path);
+        return (NULL);
+    }
 
     i = 0;
     while (path_tokens != NULL)
@@ -30,24 +38,25 @@ char *pathfinder(char *cmd)
 
     for (j = 0; path_array[j] != NULL; j++)
     {
-        _strcpy(new_path, path_array[j]);
-        _strcat(new_path, "/");
-        _strcat(new_path, s2);
-        _strcat(new_path, "\0");
+        strcpy(new_path, path_array[j]);
+        strcat(new_path, "/");
+        strcat(new_path, s2);
 
         if (stat(new_path, &buf) == 0)
         {
             if (!S_ISDIR(buf.st_mode))
             {
                 free(path);
-                return (new_path);
+                return new_path;
             }
         }
         else
+        {
             new_path[0] = '\0';
+        }
     }
 
     free(path);
     free(new_path);
-    return (NULL);
+    return NULL;
 }
