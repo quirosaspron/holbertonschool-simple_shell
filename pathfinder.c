be5571a6f71a029e800b22b026342748c43c8276
Mateo <mateoquirosaspron@gmail.com>
2023-04-30 18:46:17 -0600

    trying to make the code better

--- pathfinder.c
+++ pathfinder.c
@@ -1,51 +1,51 @@
 #include "simple_shell.h"
-/**
- * pathfinder - Takes PATH string, tokenizes it, then concats with "/" & cmd
- * @cmd: command passed from getline in main
- * Return: new_path for use in cmd_read
- */
-
 char *pathfinder(char *cmd)
 {
-	char *path = _strdup(_getenv("PATH"));
-	int i = 0, j = 0;
-	char *path_tokens = strtok(path, ":");
-	char *path_array[100];
-	char *s2 = cmd;
-	char *new_path = NULL;
-	struct stat buf;
+    struct stat buf;
 
-	new_path = malloc(sizeof(char) * 100);
-	if (_getenv("PATH")[0] == ':' && stat(cmd, &buf) == 0)
-	{
-		free(path);
-		return (_strdup(cmd));
-	}
+    if (stat(cmd, &buf) == 0)
+    {
+        if (!S_ISDIR(buf.st_mode))
+            return (_strdup(cmd));
+    }
 
-	while (path_tokens != NULL)
-	{
-		path_array[i++] = path_tokens;
-		path_tokens = strtok(NULL, ":");
-	}
-	path_array[i] = NULL;
+    char *path = _strdup(_getenv("PATH"));
+    int i = 0;
+    char *path_tokens = strtok(path, ":");
+    char *path_array[100];
+    char *s2 = cmd;
+    char *new_path = NULL;
 
-	for (j = 0; path_array[j]; j++)
-	{
-		_strcpy(new_path, path_array[j]);
-		_strcat(new_path, "/");
-		_strcat(new_path, s2);
-		_strcat(new_path, "\0");
+    new_path = malloc(sizeof(char) * 100);
 
-		if (stat(new_path, &buf) == 0)
-		{
-			free(path);
-			return (new_path);
-		}
-		else
-			new_path[0] = '\0';
-	}
+    while (path_tokens != NULL)
+    {
+        path_array[i++] = path_tokens;
+        path_tokens = strtok(NULL, ":");
+    }
+    path_array[i] = NULL;
 
-	free(path);
-	free(new_path);
-	return (NULL);
+    for (int j = 0; path_array[j]; j++)
+    {
+        _strcpy(new_path, path_array[j]);
+        _strcat(new_path, "/");
+        _strcat(new_path, s2);
+        _strcat(new_path, "\0");
+
+        if (stat(new_path, &buf) == 0)
+        {
+            if (!S_ISDIR(buf.st_mode))
+            {
+                free(path);
+                return (new_path);
+            }
+        }
+        else
+            new_path[0] = '\0';
+    }
+
+    free(path);
+    free(new_path);
+    return (NULL);
 }
+
