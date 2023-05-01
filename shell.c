6357acf898af090e830921784b72626b38de3030
Mateo <mateoquirosaspron@gmail.com>
2023-05-01 14:00:55 -0600

    revesing changes

--- shell.c
+++ shell.c
@@ -113,16 +113,6 @@ int call_command(char *cmd_arr[], char *name)
         dprintf(STDERR_FILENO, "%s: 1: %s: not found\n", name, cmd);
         status = 127;
     }
-    else if (_strcmp(cmd_arr[0], "exit") == 0)
-    {
-        if (cmd_arr[1] != NULL)
-            status = _atoi(cmd_arr[1]);
-        else
-            status = 0;
-
-        _unsetenv(&environ, "PATH");
-        _setenv(&environ, "PATH1", path_value);
-    }
     else
     {
         is_child = fork();
@@ -134,6 +124,15 @@ int call_command(char *cmd_arr[], char *name)
         else if (is_child > 0)
         {
             wait(&status);
+            if (WIFEXITED(status))
+            {
+                status = WEXITSTATUS(status);
+                if (status != 0)
+                {
+                    if (_strcmp(cmd_arr[0], "exit") == 0 && cmd_arr[1] == NULL)
+                        status = 2;
+                }
+            }
         }
         else if (is_child == 0)
         {
